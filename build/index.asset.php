5ba4b742e1b86187a949eb9af52707a13995f7a3
liquidloose <ronaldlussier83@gmail.com>
2024-12-25 08:01:26 +0000

    Initial setup complete

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '99a1a040c2da88354b7b');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2e541f1652829fa1d265');
